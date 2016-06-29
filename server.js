6a0ccbffe0712714c57fb8f59c020ddf77eceb35
Justin Allred <jjallred.ja@gmail.com>
2016-06-28 20:51:37 -0600

    just finished

---
+++ server.js
@@ -0,0 +1,45 @@
+var express = require('express');
+
+var app = express();
+
+
+
+
+var messages = ["Hello there.", "I'm sorry, I cannot take any requests at this time.", "I can tell you how to do that."];
+
+var getRandomMessage = function(messages) {
+  var randMessage = messages[Math.floor(Math.random()*messages.length)];
+  return randMessage;
+}
+
+
+app.get('/', function(req, res) {
+  res.status(200).set({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  }).send(JSON.stringify({
+    message: getRandomMessage(messages)
+  }));
+});
+
+app.options('/', function(req, res) {
+  res.status(200).set({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  }).send();
+});
+
+
+app.listen(8887, function() {
+  console.log('listening on port 8887');
+});
